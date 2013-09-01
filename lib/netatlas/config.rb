f77fcc298102a84c92bae59a2702a9c8ec671fbb
mixtli <ronmcclain75@gmail.com>
2013-08-31 19:28:37 -0700

    move log to tmp for now

--- lib/netatlas/config.rb
+++ lib/netatlas/config.rb
@@ -2,7 +2,7 @@ filename = File.dirname(__FILE__) + "/../../config/netatlas.yml"
 env = ENV['NETATLAS_ENV'] || 'production'
 NETATLAS_CONFIG = HashWithIndifferentAccess.new(YAML.load_file(filename)[env])
 
-$log = Logger.new(File.dirname(__FILE__) + "/../../log/netatlas.log")
+$log = Logger.new("/tmp/netatlas.log")
 
 NetAtlas.setup(:url => NETATLAS_CONFIG[:netatlas_url], :user => NETATLAS_CONFIG[:netatlas_user], :password => NETATLAS_CONFIG[:netatlas_password], :logger => $log)
 
